b15c3a5ed1f1718145c7bb36c6c8d44ba16f65d9
gcastillom <gissbelc@gmail.com>
2017-05-01 19:01:20 -0300

    Carpeta TiposDeDatos

---
+++ TiposDeDatos/String.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : String.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string a= "Hidro";
+	string b= "avión";
+	cout << "La concatenación es: " << a+b <<endl;
+	cout << "Su longitud es: " << (a+b).length();
+	return 0;
+}
+
